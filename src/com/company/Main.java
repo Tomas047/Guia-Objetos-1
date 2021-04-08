14812f5b136d7b9b17f390b5fbd2d09aa1b12bbb
Tomas Parant Ledesma <tomasparant@hotmail.com>
2021-04-08 17:52:51 -0300

    Clase Empleado terminada

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-
+/*
         Rectangulo x = new Rectangulo();
 
         x.setAlto(3.2);
@@ -12,5 +12,22 @@ public class Main {
 
         System.out.println(x.Area());
         System.out.println("sup, brah");
+
+ */
+
+        Empleado a = new Empleado();
+
+        a.setEmpleado("Carlos","Gutierréz","23456345",25000);
+
+        Empleado b = new Empleado();
+
+        b.setEmpleado("Ana","Sanchez","34234123",27500);
+
+        a.mostrarEmpleado();
+        b.mostrarEmpleado();
+
+        a.aumento(15);
+        System.out.println("El salario anual de "+ a.getNombre() + " es de $" + a.salarioAnual());
+
     }
 }
